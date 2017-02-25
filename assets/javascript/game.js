6a81aecaf391d29d860b74b6eae05d33d31c0e1b
Ricky Grams <rickygrams@Rickys-MacBook-Pro.local>
2017-02-24 23:31:50 -0800

    final game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,95 @@
 $(document).ready(function() {
 
-var wins = 0;
-var losses = 0;
-var randomNum = Math.floor(Math.random() * 102) + 19;
-var totalScore = 0;
-var	redCrystal = [];
-var	blueCrystal = [];
-var	yellowCrystal = [];
-var	greenCrystal = [];
+	var wins = 0;
+	var losses = 0;
+	var randomNum = Math.floor(Math.random() * 102) + 19;
+	var totalScore = 0;
+	var	redCrystal = [];
+	var	blueCrystal = [];
+	var	yellowCrystal = [];
+	var	greenCrystal = [];
 
-var crystalValues = function() {
-	redCrystal = Math.floor(Math.random() * 12) + 1;
-	blueCrystal = Math.floor(Math.random() * 12) + 1;
-	yellowCrystal = Math.floor(Math.random() * 12) + 1;
-	greenCrystal = Math.floor(Math.random() * 12) + 1;
-}
-
-function displayRanNum () {
-	$("#randomNum").html(randomNum);
-}
-
-function displayTotalScore () {
-	$("#totalScore").html(totalScore);
-}
-
-displayRanNum();
-crystalValues();
-displayTotalScore();
-
-$("#red").on("click", function() {
-	totalScore = redCrystal + totalScore;
-	displayTotalScore();
-	winLose();
-});
-
-$("#blue").on("click", function() {
-	totalScore = blueCrystal + totalScore;
-	displayTotalScore();
-	winLose();
-});
-
-$("#yellow").on("click", function() {
-	totalScore = yellowCrystal + totalScore;
-	displayTotalScore();
-	winLose();
-});
-
-$("#green").on("click", function() {
-	totalScore = greenCrystal + totalScore;
-	displayTotalScore();
-	winLose();
-});
-
-function winLose() {
-	if (totalScore === randomNum) {
-		alert("You Won!");
-		wins++;
-		$("#wins").html("Wins: " + wins);
-		reset();
+	var crystalValues = function() {
+		redCrystal = Math.floor(Math.random() * 12) + 1;
+		blueCrystal = Math.floor(Math.random() * 12) + 1;
+		yellowCrystal = Math.floor(Math.random() * 12) + 1;
+		greenCrystal = Math.floor(Math.random() * 12) + 1;
 	}
-	else if (totalScore > randomNum) {
-		alert("You Lost!");
-		losses++;
-		$("#losses").html("Losses: " + losses);
-		reset();
-	}
-}
 
-function reset () {
-    randomNum = Math.floor(Math.random() * 102) + 19;
+	document.getElementById("audio").volume = 0.009;
+	document.getElementById("audio").loop = true;
 	displayRanNum();
 	crystalValues();
-	totalScore = 0;
 	displayTotalScore();
-}
+
+	function displayRanNum () {
+		$("#randomNum").html(randomNum);
+	}
+
+	function displayTotalScore () {
+		$("#score").html(totalScore);
+	}
+
+	$("#red").on("click", function() {
+		totalScore = redCrystal + totalScore;
+		displayTotalScore();
+		winLose();
+		clearNewDiv();
+	});
+
+	$("#blue").on("click", function() {
+		totalScore = blueCrystal + totalScore;
+		displayTotalScore();
+		winLose();
+		clearNewDiv();
+	});
+
+	$("#yellow").on("click", function() {
+		totalScore = yellowCrystal + totalScore;
+		displayTotalScore();
+		winLose();
+		clearNewDiv();
+	});
+
+	$("#green").on("click", function() {
+		totalScore = greenCrystal + totalScore;
+		displayTotalScore();
+		winLose();
+		clearNewDiv();
+	});
+
+	function winLose() {
+		if (totalScore === randomNum) {	
+			$("#winLoss").append('<p id="won"></p>');
+			$("#won").html("You Won!");
+			wins++;
+			$("#wins").html("Wins: " + wins);
+			reset();	
+		}
+		else if (totalScore > randomNum) {
+			$("#winLoss").append("<p id='lost'></p>");
+			$("#lost").html("You Lost!");
+			losses++;
+			$("#losses").html("Losses: " + losses);
+			reset();
+		}
+	}
+
+	function reset () {
+	    randomNum = Math.floor(Math.random() * 102) + 19;
+		displayRanNum();
+		crystalValues();
+		totalScore = 0;
+		displayTotalScore();
+		$("#wins").html("Wins: " + wins);
+		$("#losses").html("Losses: " + losses);
+	}
+
+	function clearNewDiv() {
+		if (totalScore >= 18) {
+			$("#won").remove();
+			$("#lost").remove();
+		}
+	}
 
 });
